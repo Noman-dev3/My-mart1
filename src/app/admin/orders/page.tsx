29dab8d13df4abdb3b93a19592a8d5a857a5d76f
Python Developer <noman.dev3@gmail.com>
2025-09-09 17:46:08 +0000

    OrdersPage.useMemo[columns]@https://6000-firebase-studio-1757434852092.c

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
@@ -311,4 +312,3 @@ export default function OrdersPage() {
     </div>
   );
 }
-
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -162,7 +162,7 @@ export default function AdminDashboard() {
                         ))}
                     </Pie>
                 </PieChart>
-            </Container>
+            </ChartContainer>
           </CardContent>
         </Card>
       </div>
